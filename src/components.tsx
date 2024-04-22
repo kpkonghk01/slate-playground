cfc2132ea91a00ed2d18a3c7b8d51e602bd43f22
Pan <kpkong@hk01.com>
2024-04-22 23:32:35 +0800

    chore: remove unrelated files

--- src/components.tsx
+++ src/components.tsx
@@ -1,5 +1,4 @@
-import React, { ReactNode, Ref, PropsWithChildren } from "react";
-import ReactDOM from "react-dom";
+import React, { Ref, PropsWithChildren } from "react";
 import { cx, css } from "@emotion/css";
 
 interface BaseProps {
@@ -42,63 +41,6 @@ export const Button = React.forwardRef(
   )
 );
 
-export const EditorValue = React.forwardRef(
-  (
-    {
-      className,
-      value,
-      ...props
-    }: PropsWithChildren<
-      {
-        value: any;
-      } & BaseProps
-    >,
-    ref: Ref<HTMLDivElement>
-  ) => {
-    const textLines = value.document.nodes
-      .map((node: any) => node.text)
-      .toArray()
-      .join("\n");
-    return (
-      <div
-        ref={ref}
-        {...props}
-        className={cx(
-          className,
-          css`
-            margin: 30px -20px 0;
-          `
-        )}
-      >
-        <div
-          className={css`
-            font-size: 14px;
-            padding: 5px 20px;
-            color: #404040;
-            border-top: 2px solid #eeeeee;
-            background: #f8f8f8;
-          `}
-        >
-          Slate's value as text
-        </div>
-        <div
-          className={css`
-            color: #404040;
-            font: 12px monospace;
-            white-space: pre-wrap;
-            padding: 10px 20px;
-            div {
-              margin: 0 0 0.5em;
-            }
-          `}
-        >
-          {textLines}
-        </div>
-      </div>
-    );
-  }
-);
-
 export const Icon = React.forwardRef(
   (
     { className, ...props }: PropsWithChildren<BaseProps>,
@@ -113,28 +55,6 @@ export const Icon = React.forwardRef(
         css`
           font-size: 18px;
           vertical-align: text-bottom;
-        `
-      )}
-    />
-  )
-);
-
-export const Instruction = React.forwardRef(
-  (
-    { className, ...props }: PropsWithChildren<BaseProps>,
-    ref: Ref<HTMLDivElement>
-  ) => (
-    <div
-      {...props}
-      ref={ref}
-      className={cx(
-        className,
-        css`
-          white-space: pre-wrap;
-          margin: 0 -20px 10px;
-          padding: 10px 20px;
-          font-size: 14px;
-          background: #f8f8e8;
         `
       )}
     />
@@ -165,12 +85,6 @@ export const Menu = React.forwardRef(
     />
   )
 );
-
-export const Portal = ({ children }: { children?: ReactNode }) => {
-  return typeof document === "object"
-    ? ReactDOM.createPortal(children, document.body)
-    : null;
-};
 
 export const Toolbar = React.forwardRef(
   (
